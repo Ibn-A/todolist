814e59b7f41518cc749118de59dd646dc3eeb42e
Ibn-A <ibn_ali@yahoo.com>
2021-02-04 10:19:23 +0100

    Modification addTodo function: - ajout check fontawesome icon - Modif trashButton class name

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ function addTodo(event){
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // CHECK MARK BUTTON
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-trash"></i>';
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
     //CHECK trash BUTTON
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML == '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("complete-btn");
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     //APPEND TO LIST
     todoList.appendChild(todoDiv);
